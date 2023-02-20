0a1d596bb8e33879b70b3693f98720eef6b9668b
Arturov Kirill <kaarturov@sberbank.ru>
2023-02-20 12:30:23 +0300

    fix(commitizen): добавлен коммитайзен и хуки для хаски DS-227

--- tools/fe-scripts/configs/commitizen.js
+++ tools/fe-scripts/configs/commitizen.js
@@ -1,19 +1,19 @@
 module.exports = {
   types: [
     {
-      name: 'feat: Добавление нового функционала',
+      name: 'feat:     Добавление нового функционала',
       value: 'feat',
     },
     {
-      name: 'fix: Баг фикс',
+      name: 'fix:      Баг фикс',
       value: 'fix',
     },
     {
-      name: 'docs: Обновление документации',
+      name: 'docs:     Обновление документации',
       value: 'docs',
     },
     {
-      name: 'style: Правки по кодстайлу (табы, отступы, точки, запятые и т.д.)',
+      name: 'style:    Правки по кодстайлу (табы, отступы, точки, запятые и т.д.)',
       value: 'style',
     },
     {
@@ -21,19 +21,19 @@ module.exports = {
       value: 'refactor',
     },
     {
-      name: 'perf: Изменения направленные на улучшение производительности',
+      name: 'perf:     Изменения направленные на улучшение производительности',
       value: 'perf',
     },
     {
-      name: 'test: Добавление тестов',
+      name: 'test:     Добавление тестов',
       value: 'test',
     },
     {
-      name: 'ci: Настройка CI и работа со скриптами',
+      name: 'ci:       Настройка CI и работа со скриптами',
       value: 'ci',
     },
     {
-      name: 'chore: Build process or auxiliary tool changes',
+      name: 'chore:    Build process or auxiliary tool changes',
       value: 'chore',
     },
   ],
@@ -41,18 +41,18 @@ module.exports = {
   // scopes: [{ name: 'components' }],
   messages: {
     type: 'Какие изменения вы вносите?',
-    scope: '\nВыберите ОБЛАСТЬ, которую вы изменили (опционально):',
+    scope: 'Выберите ОБЛАСТЬ, которую вы изменили (опционально):',
     // Спросим если allowCustomScopes в true
     customScope: 'Укажите ОБЛАСТЬ изменений:',
     subject: 'Напишите КОРОТКОЕ описание:\n',
     body: 'Напишите ПОДРОБНОЕ описание (опционально). Используйте "|" для новой строки:\n',
     breaking: 'Список BREAKING CHANGES (опционально):\n',
-    footer: 'Место для мета данных (тикетов, ссылок и остального). Например: SECRETMRKT-700, SECRETMRKT-800:\n',
+    footer: 'Место для мета данных (тикетов, ссылок и остального). Например: DS-7, DS-800:\n',
     confirmCommit: 'Вас устраивает получившийся коммит?',
   },
 
   // maxHeaderWidth: 172,
-  subjectLimit: 172,
+  subjectLimit: 5,
   allowCustomScopes: true,
   // disableScopeLowerCase: true,
   // allowBreakingChanges: true,
